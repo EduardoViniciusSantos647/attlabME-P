975ed777d2a45896f1c275eda03318e6559dbd27
EduardoViniciusSantos647 <eduardoviniciussantosdelima@gmail.com>
2026-09-04 00:40:01 +0000

    Adicionar 5questao.py

---
+++ 5questao.py
@@ -0,0 +1,6 @@
+valor_compra = float(input("Digite o valor da compra: R$ "))
+
+if valor_compra >= 500:
+    print("Parabéns! Você tem direito ao desconto promocional.")
+else:
+    print("Você não tem direito ao desconto promocional.")

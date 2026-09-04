68c4d59523667a9ae5074e51a8fc3fadfee2ccd4
EduardoViniciusSantos647 <eduardoviniciussantosdelima@gmail.com>
2026-09-04 00:39:42 +0000

    Adicionar 38questao.py

---
+++ 38questao.py
@@ -0,0 +1,9 @@
+
+
+import random
+
+livros = ["Dom Casmurro", "O Cortiço", "Vidas Secas", "Iracema", "Memórias Póstumas de Brás Cubas"]
+
+livro_sorteado = random.choice(livros)
+
+print(f"O livro sorteado para leitura é: {livro_sorteado}")

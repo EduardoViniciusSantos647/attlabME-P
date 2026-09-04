ecb9685bfb7d4ec6450f10f783aa654476471e41
EduardoViniciusSantos647 <eduardoviniciussantosdelima@gmail.com>
2026-09-04 00:39:56 +0000

    Adicionar 53questao.py

---
+++ 53questao.py
@@ -0,0 +1,8 @@
+import random
+
+participantes = ["Ana", "Carlos", "Pedro", "Beatriz", "Maria"]
+
+lider = random.choice(participantes)
+
+print("Lista de participantes:", participantes)
+print("O líder sorteado para a equipe é:", lider)

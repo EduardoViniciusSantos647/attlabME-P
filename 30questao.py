f477f52e5cb0b741ddb9ef84fa8562459d5891ee
EduardoViniciusSantos647 <eduardoviniciussantosdelima@gmail.com>
2026-09-04 00:39:36 +0000

    Adicionar 30questao.py

---
+++ 30questao.py
@@ -0,0 +1,6 @@
+
+
+dias_letivos = ("Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira")
+
+print(f"Dias letivos: {dias_letivos}")
+print(f"O último dia letivo da semana é: {dias_letivos[-1]}")

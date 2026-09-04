14a0f7f2eeda25bbd99f1d6f7da68b7776e663c3
EduardoViniciusSantos647 <eduardoviniciussantosdelima@gmail.com>
2026-09-04 00:39:18 +0000

    Adicionar 10questao.py

---
+++ 10questao.py
@@ -0,0 +1,6 @@
+
+
+dias_semana = ("Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira",
+               "Sexta-feira", "Sábado", "Domingo")
+
+print(f"O primeiro dia da semana armazenado é: {dias_semana[0]}")

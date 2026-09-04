e3bdd370d3e55fab557b88fa6a9a7c3e97d2db59
EduardoViniciusSantos647 <eduardoviniciussantosdelima@gmail.com>
2026-09-04 00:39:18 +0000

    Adicionar 11questao.py

---
+++ 11questao.py
@@ -0,0 +1,8 @@
+
+
+media = float(input("Digite a média final do estudante: "))
+
+if media >= 8.0:
+    print("O estudante está APTO para participar do processo seletivo de bolsas.")
+else:
+    print("O estudante NÃO está apto para participar do processo seletivo de bolsas.")

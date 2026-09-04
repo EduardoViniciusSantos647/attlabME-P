0924a20e216e27bf67ed049166abe53ca95ee479
EduardoViniciusSantos647 <eduardoviniciussantosdelima@gmail.com>
2026-09-04 00:39:44 +0000

    Adicionar 3questao.py

---
+++ 3questao.py
@@ -0,0 +1,6 @@
+setor1 = float(input("Digite a quantidade produzida pelo Setor 1: "))
+setor2 = float(input("Digite a quantidade produzida pelo Setor 2: "))
+
+total = setor1 + setor2
+
+print(f"A produção total do dia foi de {total} de hortaliças.")

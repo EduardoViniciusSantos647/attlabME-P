526363ef8360e3cdcb6749142f192ca049f6787a
EduardoViniciusSantos647 <eduardoviniciussantosdelima@gmail.com>
2026-09-04 00:39:23 +0000

    Adicionar 17questao.py

---
+++ 17questao.py
@@ -0,0 +1,7 @@
+total = 0
+
+for i in range(10):
+    quantidade = int(input("Digite a quantidade de cestas arrecadadas: "))
+    total = total + quantidade
+
+print("Total de cestas arrecadadas:", total)

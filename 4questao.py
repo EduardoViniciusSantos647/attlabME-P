62c40cfaf7c1c8efc8cd883ad5d7979c302d350d
EduardoViniciusSantos647 <eduardoviniciussantosdelima@gmail.com>
2026-09-04 00:39:53 +0000

    Adicionar 4questao.py

---
+++ 4questao.py
@@ -0,0 +1,8 @@
+
+
+nota1 = float(input("Digite a primeira nota do estudante: "))
+nota2 = float(input("Digite a segunda nota do estudante: "))
+
+media = (nota1 + nota2) / 2
+
+print(f"A média do estudante é {media}.")
